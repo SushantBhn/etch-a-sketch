f28691dbdd932b5d69702712fecd2e7eda3d5fa0
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-02-27 22:17:20 +0545

    Set up a hover effect to emulate a sketchpad

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ for  (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     container.appendChild(div);
 }
+
+const divs = document.querySelectorAll(".container div");
+
+divs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "red";
+    });
+});
