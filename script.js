53ece48fc4afba5f1949946e5c004e7a0182413b
Sushant Bhandari <133732265+SushantBhn@users.noreply.github.com>
2025-02-28 11:50:26 +0545

    Add the UI to change the grid size dynamically
    The user can now input the size of the desired square grid

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const container = document.querySelector(".container");
+function createGrid (gridLength = 16) {
+    const container = document.querySelector(".container");
+    container.style.setProperty("--grid-length", gridLength);
 
-for  (let i = 0; i < 256; i++) {
-    let div = document.createElement("div");
-    container.appendChild(div);
+    for  (let i = 0; i < gridLength * gridLength; i++) {
+        let div = document.createElement("div");
+        container.appendChild(div);
+        // div.style.width =  "calc(100% / gridLength)";
+
+    }
 }
 
-const divs = document.querySelectorAll(".container div");
+createGrid();
+hoverEffect();
 
-divs.forEach((div) => {
-    div.addEventListener("mouseover", () => {
-        div.style.backgroundColor = "red";
+function hoverEffect() {
+    const divs = document.querySelectorAll(".container div");
+
+    divs.forEach((div) => {
+        div.addEventListener("mouseover", () => {
+            div.style.backgroundColor = "red";
+        });
     });
-});
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let gridLength = prompt("Enter the number of squares per side for the new grid:", 16);
+
+    //Delete the container div
+    const container = document.querySelector(".container");
+    document.body.removeChild(container);
+
+    //Create a new container div
+    let newContainer = document.createElement("div");
+    document.body.appendChild(newContainer);
+    newContainer.classList.add("container");
+    
+    createGrid(gridLength);
+    hoverEffect();
+})
